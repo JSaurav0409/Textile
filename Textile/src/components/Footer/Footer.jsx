4a07ad9a61db77fa1075c3b3b65fc4a0bd9fbfae
Saurav Jha <sauravjha0409@gmail.com>
2025-01-09 17:37:01 +0530

    minor changes

--- Textile/src/components/Footer/Footer.jsx
+++ Textile/src/components/Footer/Footer.jsx
@@ -1,22 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-600 text-white p-6 mt-10">
+    <footer className="bg-gray-800 text-white p-6 mt-10">
       <div className="container mx-auto text-center">
         <p className="text-sm sm:text-base md:text-lg">
           &copy; 2025 Textile Company. All rights reserved.
         </p>
         <div className="mt-4">
-          <a
-            href="#"
-            className="text-white hover:text-gray-200 mx-2 text-xs sm:text-sm md:text-base"
+          <NavLink
+            to="/privacy-policy"
+            className="text-white hover:text-gray-400 mx-2 text-xs sm:text-sm md:text-base"
           >
             Privacy Policy
-          </a>
+          </NavLink>
           <a
             href="#"
-            className="text-white hover:text-gray-200 mx-2 text-xs sm:text-sm md:text-base"
+            className="text-white hover:text-gray-400 mx-2 text-xs sm:text-sm md:text-base"
           >
             Terms of Service
           </a>
