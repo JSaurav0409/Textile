c6faad34b968ec9542317cfcc84156b24f1a1dc2
Saurav Jha <sauravjha0409@gmail.com>
2025-01-09 18:59:58 +0530

    Move to other folder

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Team from "../About/Team";
-import CompanyHistory from "../About/CompanyHistory";
+import Team from "./Team";
+import CompanyHistory from "./CompanyHistory";
 
 const About = () => {
   // Initialize AOS on component mount
